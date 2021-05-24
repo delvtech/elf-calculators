2769afa6c215b6cc2f05f74ca17e3d24ef9e19c8
jonny rhea <jonathan.rhea@gmail.com>
2021-05-24 14:55:34 -0500

    remove erroneous variable x_start and update with time_stretch calc and total supply

--- ytc/js/ytc.js
+++ ytc/js/ytc.js
@@ -1,17 +1,15 @@
 "use strict";
-function calculateRow(speculated_interest=10.5,input_amount=10,
+function calculateRow(speculated_interest=10,input_amount=10,
                days_maturity=90,days_matured=0,
-               liquidity=50,time_stretch=15,
-               target_interest=16) {
+               liquidity=50,time_stretch=11,
+               target_interest=20) {
     let t = (days_maturity-days_matured) / (365 * time_stretch)
     let T = (days_maturity-days_matured) / 365
-    let y_start = liquidity
     let g = .1
     let gas_fee=0.0019
     let accumulated_interest = input_amount * days_matured/365 * speculated_interest/100
     let A = ((input_amount)*(speculated_interest/100)*T-(target_interest/1000)*input_amount*T-gas_fee)/input_amount
     let resulting_pt_apy = A/T*100
-    let x_start=y_start/((1-T*(resulting_pt_apy/100))**(-1/t)-1)
     let unit_price = 1-A
     let expenditure=input_amount-unit_price*input_amount+gas_fee+accumulated_interest
     let received = (input_amount * speculated_interest / 100) * (days_maturity) / 365
@@ -23,8 +21,8 @@ function calculateRow(speculated_interest=10.5,input_amount=10,
     //calculate pre-slippage
     let pt_apy = (1- output_amount/input_amount)/T * 100 
     let y_after=liquidity+input_amount
-    let x_after=y_after/((1-T*(pt_apy/100))**(-1/t)-1)
-    let total_supply = x_after
+    let x_after=y_after*(-(2/((1-T*pt_apy/100)**(1/t)-1))-2)
+    let total_supply = x_after+y_after
     let k = (y_after + total_supply)**(1-t) + x_after**(1-t)
     let fj = input_amount 
     let fi = y_after + total_supply - Math.pow(k-Math.pow(x_after + fj,1-t),1/(1-t))
@@ -72,7 +70,7 @@ const liquidityElement = document.getElementById("liquidity");
 
 calculate.addEventListener('click', (event) => {
     let data = []
-    let time_stretch = 297.80126 /( 4.32257 * parseFloat(speculatedApyElement.value))
+    let time_stretch = 3.09396 /( 0.02789 * parseFloat(speculatedApyElement.value))
     let start=0
     let end=0
     let increment=0
